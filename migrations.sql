7438857f8ad0c6cacc76223a1967d969afe3d7c6
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-11-21 15:17:52 +0200

    first commit

---
+++ migrations.sql
@@ -0,0 +1,21 @@
+-- DROP DATABASE users;
+-- CREATE DATABASE sipols;
+-- USE sipols;
+-- CREATE TABLE users ( 
+/*id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+username VARCHAR(12)NOT NULL,
+`password` VARCHAR(50)NOT NULL
+);
+INSERT INTO users
+(username, `password`)
+VALUES
+("slopis","parole123");
+
+SELECT * FROM users;
+
+CREATE TABLE stars(
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+user_id INT NOT NULL,
+image VARCHAR (1000)NOT NULL
+);
+*/
